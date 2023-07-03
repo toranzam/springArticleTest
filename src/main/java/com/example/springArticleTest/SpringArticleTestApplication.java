c769a4da92ab479d852ef4b7a283714741333dd4
nbc05829 <nbc05829@gmail.com>
2023-07-03 15:07:40 +0900

    first commit

---
+++ src/main/java/com/example/springArticleTest/SpringArticleTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springArticleTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArticleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringArticleTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springArticleTest/SpringArticleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springArticleTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringArticleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
